c7133d0ff79434d79fd4d02b057350eaf84389eb
AMOREM <amorem@amorem.com>
2019-05-19 12:14:54 +0200

    Replace App Icon in ios and android

--- lib/main.dart
+++ lib/main.dart
@@ -10,9 +10,7 @@ void main() {
         ),
         backgroundColor: Colors.blueGrey,
         body: Center(
-          child: Image(
-              image: NetworkImage(
-                  'https://www.w3schools.com/w3css/img_lights.jpg')),
+          child: Image(image: AssetImage('images/diamond.png')),
         ),
       ),
     ),
